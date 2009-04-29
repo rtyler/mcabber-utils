b2d987f622bc9a2eab1706019d11ba4a50df4b4c
Travis Parker <travis@slide.com>
2009-04-28 18:51:53 -0700

    clear old events when we start the notification system

--- start.sh
+++ start.sh
@@ -8,5 +8,6 @@ pidtail() {
 }
 
 echo -n > $DIR/notify_pids
+echo -n > $DIR/last_event
 (pidtail $DIR/last_event 2>> $DIR/notify_pids | python $DIR/mcabbernotify.py 2>> $DIR/notify_pids) &
 echo $! >> $DIR/notify_pids
